de947ebf5625efb8d647646e3f776db85c25d93d
SourabhPatidar711 <sourabhnote9@gmail.com>
2025-03-29 22:12:55 +0530

    Add frontend/src/App.tsx 1743266573863

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DisasterMapDashboard from './components/DisasterMapDashboard';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <DisasterMapDashboard />
+    </div>
+  );
+}
+
+export default App;
